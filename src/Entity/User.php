6eea03590250258d321f83855e70d3eb837005bd
cremedemarron <stivaltheo@gmail.com>
2021-07-19 16:31:28 +0200

    seconde commit

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,11 +4,13 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
  */
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
@@ -70,6 +72,11 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     private $timeToCreate;
 
+
+    function __toString()
+    {
+        return $this -> avatar;
+    }
     public function getId(): ?int
     {
         return $this->id;
